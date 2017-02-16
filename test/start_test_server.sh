4402a04c479569640a136a51a7b68d719eb416df
David <ottodavid@gmx.net>
2017-02-16 07:37:42 +0100

    improve test server script

--- test/start_test_server.sh
+++ test/start_test_server.sh
@@ -1,14 +1,22 @@
-#!/bin/sh
+#!/bin/bash
+# Starts an in-memory-ldap browser with an predefined set of test dataa
 
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ZIPFILE="${SCRIPTDIR}/unboundid.zip"
+TESTFILE="${SCRIPTDIR}/testdata.ldif"
 
-if [ ! -e unboundid-ldapsdk*/tools/in-memory-directory-server ]; then
-  curl "https://docs.ldap.com/ldap-sdk/files/unboundid-ldapsdk-3.1.0-se.zip" -o unboundid.zip
-  unzip -o unboundid.zip
+cd "${SCRIPTDIR}"
+
+if [ ! -e $SCRIPTDIR/unboundid-ldapsdk*/tools/in-memory-directory-server ]; then
+  curl "https://docs.ldap.com/ldap-sdk/files/unboundid-ldapsdk-3.1.0-se.zip" -o "${ZIPFILE}"
+  unzip -o ${ZIPFILE}
 fi
 
-cd unboundid-ldap*
-curl "https://raw.githubusercontent.com/ruby-ldap/ruby-net-ldap/master/test/testdata.ldif" -o testdata.ldif
+[ ! -e "${TESTFILE}" ] && curl "https://raw.githubusercontent.com/ruby-ldap/ruby-net-ldap/master/test/testdata.ldif" -o "${TESTFILE}"
+
+cd unboundid-ldapsdk*
+
 echo connect with ldapbrowse -p 1234 -b "dc=bayshorenetworks,dc=com"
 ./tools/in-memory-directory-server --baseDN "dc=bayshorenetworks,dc=com" \
      --port 1234 \
-     --ldifFile testdata.ldif 
+     --ldifFile $TESTFILE
